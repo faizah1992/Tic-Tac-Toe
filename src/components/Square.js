b16340124e903618938f978166af80841be9b464
Faizah Ahsan <faizah1992@gmail.com>
2020-11-13 20:28:28 -0600

    updated square file

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,7 +1,8 @@
 import React from 'react';
 
 const style = {
-    background: 'lightblue'
+    background: 'lightblue',
+    border: '2px solid darkblue',
 }
 const Square = ({value, onClick}) => (
 
