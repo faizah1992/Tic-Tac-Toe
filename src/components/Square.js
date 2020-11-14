c525a6ad687b4f68bad1961333deb458653f1e49
Faizah Ahsan <faizah1992@gmail.com>
2020-11-13 20:31:48 -0600

    updated square file

--- src/components/Square.js
+++ src/components/Square.js
@@ -5,6 +5,8 @@ const style = {
     border: '2px solid darkblue',
     fontSize: '30px',
     fontWeight: '800',
+    cursor: 'pointer',
+    outline: 'none'
 }
 const Square = ({value, onClick}) => (
 
