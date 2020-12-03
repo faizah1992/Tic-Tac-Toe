6b72ea81c96a4152772b3d34a3501a28eb62bc8d
Faizah Ahsan <faizah1992@gmail.com>
2020-12-03 13:24:11 -0600

    game js updated

--- src/components/Game.js
+++ src/components/Game.js
@@ -33,6 +33,9 @@ const Game= () => {
     return (
         <>
         <Board  squares={board}  onClick={handleClick}/>
+        <div style={styles}>
+            <p>{winner ? 'Winner' : winner : 'Next Player: ' + (xIsNext ? 'X' : 'O')}</p>
+        </div>
         </>
     )
 }
