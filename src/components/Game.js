6ac65e4fa793b08ab2c82e972802790d5ef55805
Faizah Ahsan <faizah1992@gmail.com>
2020-12-03 13:20:12 -0600

    style added

--- src/components/Game.js
+++ src/components/Game.js
@@ -2,6 +2,10 @@ import React, {useState} from 'react';
 import { calculateWinner } from '../helpers'
 import Board from './Board'
 
+const styles = {
+    width: '200px',
+}
+
 const Game= () => {
     const [board, setBoard] = useState(Array(9).fill(null))
 
@@ -26,7 +30,9 @@ const Game= () => {
     }
     
     return (
+        <>
         <Board  squares={board}  onClick={handleClick}/>
+        </>
     )
 }
 
